71089afe5a8cacad8baaad09f8949564ebe846be
Nwannadi Emeke <emekedev@gmail.com>
2025-06-14 01:09:50 +0100

    quiz.js updated

---
+++ quiz.js
@@ -0,0 +1,14 @@
+function checkAnswer(){
+    let correctAnswer = "4";
+    let inputValue = document.querySelector('input[name="quiz"]:checked');
+    let userAnswer = inputValue.value;
+
+    if(userAnswer === correctAnswer){
+        document.getElementById('feedback').textContent = 'Correct! Well done.'
+    }else{
+        document.getElementById('feedback').textContent = `That's incorrect. Try again!`
+    }
+
+    let btn = document.getElementById('submit-answer');
+    btn.addEventListener('click', checkAnswer)
+}
